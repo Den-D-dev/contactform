0c390c68058f656ed77ff72b007a3b9e29ad92d6
Dennis Mgbah <dennismgbah@gmail.com>
2020-03-23 11:06:27 +0100

    wrote a code that generate a txt file with the registered person's details

--- processform.php
+++ processform.php
@@ -1,6 +1,38 @@
 <?php
 
-print_r($_POST)
+if($_POST['terms'] == "accepted") {
 
+print_r($_POST);
+
+$first_name = $_POST['first_name'];
+$last_name = $_POST['last_name'];
+$gender = $_POST['gender'];
+$email = $_POST['email'];
+$text = $_POST['message'];
+$location = $_POST['location'];
+$department = $_POST['department'];
+$interest = $_POST['interest'];
+
+
+// save person's info into a file
+
+$message = "Dear " . $first_name . ", \rHere are the details you entered below: \r\r";
+
+$message .= "First Name : " . $first_name . "\r";
+$message .= "Last Name : " . $last_name . "\r";
+$message .= "Gender : " . $gender . "\r";
+$message .= "Email : " . $email . "\r";
+$message .= "Location : " . $location . "\r";
+$message .= "Department : " . $department . "\r";
+$message .= "Interest : " . $interest . "\r";
+$message .= "Your Message : " . $text;
+
+$personFile = fopen($first_name . ".txt", "w") or die("Unable to open file!");
+fwrite($personFile, $message);
+fclose($personFile);
+
+} else {
+    echo " You must accept our terms and conditions";
+}
 
 ?>
